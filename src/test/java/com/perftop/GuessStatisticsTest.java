5557bc1bb1e2f6bb2fbd36a8c937cca20ca6a771
Artie Shevchenko <ashevchenko@intoperf.com>
2023-11-12 13:41:04 +1100

    Uncle Bob: "Provide a clear context for the three variables". Really?

--- src/test/java/com/perftop/GuessStatisticsTest.java
+++ src/test/java/com/perftop/GuessStatisticsTest.java
@@ -1,24 +1,31 @@
 package com.perftop;
 
-import static com.perftop.GuessStatistics.getGuessStatisticsMessage;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GuessStatisticsTest {
 
+  GuessStatistics guessStatistics;
+
+  @Before
+  public void setUp() {
+    guessStatistics = new GuessStatistics();
+  }
+
   @Test
   public void zeroMessage() {
-    assertEquals("There are no bs", getGuessStatisticsMessage('b', 0));
+    assertEquals("There are no bs", guessStatistics.getGuessStatisticsMessage('b', 0));
   }
 
   @Test
   public void oneMessage() {
-    assertEquals("There is 1 b", getGuessStatisticsMessage('b', 1));
+    assertEquals("There is 1 b", guessStatistics.getGuessStatisticsMessage('b', 1));
   }
 
   @Test
   public void twoMessage() {
-    assertEquals("There are 2 bs", getGuessStatisticsMessage('b', 2));
+    assertEquals("There are 2 bs", guessStatistics.getGuessStatisticsMessage('b', 2));
   }
 }
